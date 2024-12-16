08c8e2ef628fda9135128ad1135311ccfabd6e94
Your Name <nathanielbd@gmail.com>
2024-12-15 23:58:31 -0600

    13 done

--- 13/second.py
+++ 13/second.py
@@ -2,6 +2,7 @@ import numpy as np
 
 A_PRICE = 3
 B_PRICE = 1
+PRIZE_OFFSET = 10000000000000
 
 f = open('input.txt', 'r')
 data = [row.strip() for row in f if row.strip() != '']
@@ -16,10 +17,12 @@ for button_a, button_b, prize in zip(data[::3], data[1::3], data[2::3]):
     A = np.array([[a_x, b_x],
                   [a_y, b_y]])
     p_x, p_y = parse_info(prize, '=')
+    p_x += PRIZE_OFFSET
+    p_y += PRIZE_OFFSET
     b = np.array([p_x,
                   p_y])
     sol = np.linalg.solve(A, b)
-    cond = np.all(np.isclose(sol, np.rint(sol)))
+    cond = np.all(np.dot(A, np.rint(sol)) == b)
     if cond:
         total += sol[0]*A_PRICE + sol[1]*B_PRICE
 print(int(total))
